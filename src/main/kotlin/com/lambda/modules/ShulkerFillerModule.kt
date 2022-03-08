17779e0c6e33508a3969467f3603e9fb9651a379
luigit12 <58264257+Luigit12@users.noreply.github.com>
2022-03-08 20:32:40 +0100

    a

--- src/main/kotlin/com/lambda/modules/ShulkerFillerModule.kt
+++ src/main/kotlin/com/lambda/modules/ShulkerFillerModule.kt
@@ -3,19 +3,24 @@ package com.lambda.modules
 import baritone.api.pathing.goals.GoalXZ
 import com.lambda.ShulkerFiller
 import com.lambda.client.event.SafeClientEvent
+import com.lambda.client.event.events.PacketEvent
 import com.lambda.client.event.events.PlayerAttackEvent
 import com.lambda.client.mixin.extension.syncCurrentPlayItem
 import com.lambda.client.module.Category
 import com.lambda.client.plugin.api.PluginModule
 import com.lambda.client.util.BaritoneUtils
+import com.lambda.client.util.EntityUtils
 import com.lambda.client.util.Wrapper.player
 import com.lambda.client.util.Wrapper.world
 import com.lambda.client.util.combat.CombatUtils
 import com.lambda.client.util.combat.CombatUtils.equipBestWeapon
+import com.lambda.client.util.graphics.LambdaTessellator
 import com.lambda.client.util.items.hotbarSlots
 import com.lambda.client.util.items.swapToSlot
 import com.lambda.client.util.text.MessageSendHelper
+import com.lambda.client.util.threads.runSafe
 import com.lambda.client.util.threads.safeListener
+import net.minecraft.block.Block
 import net.minecraft.block.state.IBlockState
 import net.minecraft.enchantment.EnchantmentHelper
 import net.minecraft.entity.EntityLivingBase
@@ -23,6 +28,12 @@ import net.minecraft.init.Enchantments
 import net.minecraftforge.event.entity.player.PlayerInteractEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent
 import org.lwjgl.input.Mouse
+import net.minecraft.entity.item.EntityItem
+import net.minecraft.entity.Entity
+import net.minecraft.entity.EntityList
+import net.minecraft.init.Items
+import net.minecraft.world.World
+import scala.swing.`UIElement$class`.location
 
 /**
  * This is a module. First set properties then settings then add listener.
@@ -35,20 +46,32 @@ internal object ShulkerFillerModule : PluginModule(
 ) {
 
     init {
-        onEnable {
-            startPathing()
-            MessageSendHelper.sendChatMessage("Enabled ShulkerFiller")
+        //safeListener<PacketEvent.Receive> {
+            onEnable {
+                MessageSendHelper.sendChatMessage("Enabled ShulkerFiller")
+                //System.out.println(Block.getBlockFromName("minecraft:chest"))
+                //System.out.println(world?.getLoadedEntityList())
+                runSafe { startPathing() }
+
+            }
+            onDisable {
+                BaritoneUtils.cancelEverything()
+                MessageSendHelper.sendChatMessage("Disabled ShulkerFiller")
+            }
         }
-        onDisable {
+
+        private fun SafeClientEvent.startPathing() {
+            if (player.let { it.let { it1 -> world.isChunkGeneratedAt(it1.chunkCoordX, player.chunkCoordZ) } }) return
+
+            val entities = world.getLoadedEntityList()
+            val gappleEntity = entities.filterIsInstance<EntityItem>().filter { it == Items.GOLDEN_APPLE }
+
+            System.out.println(gappleEntity)
+
+            //[EntityItem['item.tile.obsidian'/1001338, l='MpServer', x=189206.36, y=63.00, z=40704.19]
+
             BaritoneUtils.cancelEverything()
-            MessageSendHelper.sendChatMessage("Disabled ShulkerFiller")
+            //BaritoneUtils.primary?.customGoalProcess?.setGoalAndPath(GoalXZ(10, 10))
         }
-    }
-
-    private fun startPathing() {
-        if (!player?.let { world?.isChunkGeneratedAt(it.chunkCoordX, player!!.chunkCoordZ) }!!) return
-
-        BaritoneUtils.cancelEverything()
-        BaritoneUtils.primary?.customGoalProcess?.setGoalAndPath(GoalXZ(189209, 40719))
-    }
+//    }
 }
